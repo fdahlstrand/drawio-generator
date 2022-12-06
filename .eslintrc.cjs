8e02502a6db5b149ed7c8f0d31b0edc8af1b2997
Fredrik Dahlstrand <fredrik.dahlstrand@outlook.com>
2022-12-06 21:43:24 +0100

    Enable linting with type information

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,20 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: "**/*.ts*",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
